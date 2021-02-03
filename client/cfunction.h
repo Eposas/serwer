79db6aa25c66e1b4d907bafebf75aec4345cb79a
Eposas <medonmat1234@gmail.com>
2021-02-03 18:56:28 +0100

    commit 2

--- client/cfunction.h
+++ client/cfunction.h
@@ -10,18 +10,51 @@
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <signal.h>
+#include <sys/time.h>
 
 
+float decompose=0;
+int owning=0;
 
-
-void getOpt(int argc, char* argv[], int* storage, float* tempo, float*  decompose, char* o_arg);
+void getOpt(int argc, char* argv[], int* storage, float* tempo,  char* o_arg);
 in_addr_t isAddrOk(char* o_arg,  char* port, char* host);
-void calcvalue(int* storage, float* tempo, float* decompose);
-void sleep_after_comsumpcion(float tempo);
-void sleep_decompose(struct timespec start, struct timespec stop, float decompose, int* mam);
-void catcher(int sig);
+void calcvalue(int* storage, float* tempo);
+void sleep_after_comsumpcion(float tempo, double time);
+//void catcher(int sig);
 void exitfunction(int status, void* message);
-double calc_time(struct timespec t1, struct timespec t2);
+double calc_time(struct timespec t2, struct timespec t1);
+//void set_alarm();
+void raport();
+
+
+void raport(char* adr, int port){
+    char text[100];
+    struct timespec real;
+    clock_gettime(CLOCK_REALTIME, &real);
+    int PID=getpid();
+    sprintf(text, "PID: %d TS: %ld %ld %s %d\n", PID, real.tv_sec, real.tv_nsec, adr, port);
+
+    int rr=on_exit(exitfunction, (void*)text);
+    if(rr!=0){
+        fprintf(stderr, "cannot set exit function");
+        exit(1);
+    }
+}
+
+/*
+void set_alarm(){
+    struct sigaction sack;
+    sigemptyset(&sack.sa_mask);
+    sack.sa_flags=0;
+    sack.sa_handler=catcher;
+    sigaction(SIGALRM, &sack, NULL);
+
+    struct itimerval new;
+    new.it_interval.tv_sec=1, new.it_interval.tv_usec=1, new.it_value.tv_usec=0, new.it_value.tv_sec=1;
+    struct itimerval old;
+    old.it_interval.tv_sec=1, old.it_interval.tv_usec=1, old.it_value.tv_sec=1, old.it_value.tv_usec=0;
+    setitimer(SIGALRM, &new, &old);
+}*/
 
 
 void exitfunction(int status, void* message){
@@ -30,42 +63,46 @@ void exitfunction(int status, void* message){
 }
 
 
-double calc_time(struct timespec t1, struct timespec t2)
+double calc_time(struct timespec t2, struct timespec t1)
 {
-   double val= t1.tv_sec - t1.tv_sec + (t2.tv_nsec - t1.tv_nsec) / 1000000000.0;
-   return val;
+    long long sec, nsec;
+    if(t2.tv_sec==t1.tv_sec)
+    {
+        sec=0;
+        nsec=t2.tv_nsec-t1.tv_nsec;
+    }
+    else
+    {
+        long int h1=t1.tv_sec*1000000000+t1.tv_nsec;
+        long int h2=t2.tv_sec*1000000000+t2.tv_nsec;
+        long int wynik=h2-h1;
+        sec=wynik/1000000000;
+        nsec=wynik-sec*1000000000;
+    }
+    double res=sec+(double)nsec/1000000000;
+    printf(" res %f\n", res);
+    return res;
 }
 
-void sleep_decompose(struct timespec start, struct timespec stop, float decompose, int* mam){
+void sleep_after_comsumpcion(float tempo, double time) {
 
-    double value=stop.tv_sec - start.tv_sec + (stop.tv_nsec - start.tv_nsec) / 1000000000.0;
-    int del=(int)(value*decompose);
-    *mam=*mam-del;
-    if(*mam<0) *mam=0;
-    printf("czas petli %f niszcze %d\n", value, del);
-    alarm((int)value);
-}
-
-void catcher(int sig){
-    printf("signal cought %d \n", sig);
-}
-
-void sleep_after_comsumpcion(float tempo) {
     struct timespec sleeptime;
-    struct timespec wake;
-    long int tmp = 1024 / (tempo * 4435) * 1000000000;
-    sleeptime.tv_nsec = tmp % 1000000000;
-    sleeptime.tv_sec = tmp - (tmp % 1000000000);
-    printf("spie %ld \n", tmp);
+    struct timespec wake={wake.tv_sec=0, wake.tv_nsec=0};
+    double tmp = (13*1024)/tempo;
+    sleeptime.tv_sec = (int)tmp;
+    sleeptime.tv_nsec = (tmp-(int)tmp)*1000000000;
     nanosleep(&sleeptime, &wake);
+    owning=owning-(int)(time*decompose);
+    if(owning<0) owning=0;
+    if(wake.tv_nsec!=0 || wake.tv_sec!=0) exit(1);
 }
-void calcvalue(int* storage, float* tempo, float* decompose){
+
+void calcvalue(int* storage, float* tempo){
 
     *storage=*storage*30*1024;
     *tempo=*tempo*4435;
-    *decompose=*decompose*819;
+    decompose=decompose*819;
 }
-
 
 in_addr_t isAddrOk(char* o_arg,  char* port, char* host)
 {
@@ -78,7 +115,7 @@ in_addr_t isAddrOk(char* o_arg,  char* port, char* host)
     return strtol(port, NULL, 0);
 }
 
-void getOpt(int argc, char* argv[], int* storage, float* tempo, float*  decompose, char* o_arg){
+void getOpt(int argc, char* argv[], int* storage, float* tempo, char* o_arg){
     if((argc<7))
     {
         fprintf(stderr, "Not enough args %s\n", argv[0]);
@@ -104,8 +141,8 @@ void getOpt(int argc, char* argv[], int* storage, float* tempo, float*  decompos
                 }
                 break;
             case 'd':
-                *decompose=strtof(optarg, &endptr);
-                if(*decompose<=0){
+                decompose=strtof(optarg, &endptr);
+                if(decompose<=0){
                     fprintf(stderr, "value <=0 is not acceptable");
                     exit(2);
                 }
--- client/main.c
+++ client/main.c
@@ -4,17 +4,14 @@
 int main(int argc, char* argv[]) {
 
     char *o_arg = calloc(64, sizeof(char));
-    float decompose=0;
     float tempo = 0;
     int storage=0;
     char *host = calloc(32, sizeof(char));
     char *port_temp = calloc(32, sizeof(char));
-    struct timespec time_start;
-    struct timespec time_stop;
 
-    getOpt(argc, argv, &storage, &tempo, &decompose, o_arg);
+    getOpt(argc, argv, &storage, &tempo, o_arg);
     in_port_t port=isAddrOk(o_arg, port_temp, host);
-    calcvalue(&storage, &tempo, &decompose);
+    calcvalue(&storage, &tempo);
     char* adr="127.0.0.1";
     if(strcmp("localhost", host)==0) {
         strcpy(host, adr);
@@ -22,25 +19,20 @@ int main(int argc, char* argv[]) {
     }
 
     printf("storage %d, tempo %f, decompose %f, host %s, port %d \n", storage, tempo, decompose, host, port);
-    struct sigaction sack;
-    sigemptyset(&sack.sa_mask);
-    sack.sa_flags=0;
-    sack.sa_handler=catcher;
-    sigaction(SIGALRM, &sack, NULL);
-    int owning=0;
+    //set_alarm();
+    struct timespec start;
+
+    clock_gettime(CLOCK_REALTIME, &start);
 
         while (1) {
-            clock_gettime(CLOCK_REALTIME, &time_start);
-            if(owning >= storage-13*1024){
-                fprintf(stderr, "babciu więcej już nie zmieszcze %d %d", owning, storage);
-                exit(1);
-            }
-            printf("posiadam %d\n", owning);
+            struct timespec  stop, time_start/*, time_stop, time_rel*/;
             int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
             if (sock_fd == -1) {
                 fprintf(stderr, "client cannot create socket");
                 exit(2);
             }
+
+            printf("posiadam %d\n", owning);
 
             struct sockaddr_in addresstruct;
             addresstruct.sin_family = AF_INET;
@@ -51,11 +43,13 @@ int main(int argc, char* argv[]) {
                 fprintf(stderr, "uncorret serwer addres");
                 exit(2);
             }
-
             int proba = 11;
             while (--proba) {
                 if (connect(sock_fd, (struct sockaddr *) &addresstruct, sizeof(addresstruct)) != -1) break;
             }
+
+           clock_gettime(CLOCK_MONOTONIC, &time_start);
+
             if (!proba) {
                 fprintf(stderr, "connection not accepted");
                 exit(2);
@@ -70,20 +64,37 @@ int main(int argc, char* argv[]) {
                     i++;
                     owning+=1024;
                 }
+              //  if(i==0)  clock_gettime(CLOCK_MONOTONIC, &time_rel);
             }
-            sleep_after_comsumpcion(tempo);
-            clock_gettime(CLOCK_REALTIME, &time_stop);
-            sleep_decompose(time_start, time_stop, decompose, &owning);
+
+            clock_gettime(CLOCK_REALTIME, &stop);
+            sleep_after_comsumpcion(tempo, calc_time(stop, start));
+            start.tv_nsec=stop.tv_nsec;
+            start.tv_sec=stop.tv_sec;
+
+          //  clock_gettime(CLOCK_MONOTONIC, &time_stop);
+           /* double a=calc_time(&time_rel ,&time_start);
+            double b=calc_time(&time_stop ,&time_rel );*/
+
+          /*  char eachblock[100];
+            sprintf(eachblock, "connection %f \n", calc_time(time_rel, time_start));
+            int r=on_exit(exitfunction, (void*)eachblock);
+            if(r!=0){
+                fprintf(stderr, "cannot set exit function");
+                exit(1);
+            }*/
+            if(owning >= storage-13*1024){
+                close(sock_fd);
+                raport(adr, port);
+                exit(0);
+            }
+
             if (close(sock_fd) == -1) {
                 fprintf(stderr, "\nklopoty\n");
                 exit(1);
             }
-            char* message="dsa";
-            int r=on_exit(exitfunction, (void*)message);
-            if(r!=0){
-                fprintf(stderr, "cannot set exit function");
-                exit(1);
-            }
+
+
 
         }
     }
